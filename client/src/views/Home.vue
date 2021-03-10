8c33fc7ee4138880b034c06dc20db4a6e27eed1f
Kirill Savko <kirillsavko25@gmail.com>
2021-03-10 16:34:49 +0300

    feat: add tailwind

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
-@Options({
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
   components: {
     HelloWorld,
   },
-})
-export default class Home extends Vue {}
+});
 </script>
